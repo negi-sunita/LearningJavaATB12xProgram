3910716b90d6468ea3a796f708bc1035e8a5bf76
sunitanegi <sunita.negi@gmail.com>
2025-05-07 23:11:35 +0530

    Hello Ansh

---
+++ src/ex_01_Java_Basics/HelloAnsh.java
@@ -0,0 +1,7 @@
+package ex_01_Java_Basics;
+
+public class HelloAnsh {
+    public static void main(String[] args) {
+        System.out.println("Hello Ansh,\n\nHow are you my son?\n \nLove you ");
+    }
+}
