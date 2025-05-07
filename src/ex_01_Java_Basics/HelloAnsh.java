bde3e789f657af4b123be6a9d8beea14efca7821
sunitanegi <sunita.negi@gmail.com>
2025-05-07 23:46:53 +0530

    Hello Ansh

--- src/ex_01_Java_Basics/HelloAnsh.java
+++ src/ex_01_Java_Basics/HelloAnsh.java
@@ -3,5 +3,12 @@ package ex_01_Java_Basics;
 public class HelloAnsh {
     public static void main(String[] args) {
         System.out.println("Hello Ansh,\n\nHow are you my son?\n \nLove you ");
+
+        int num1 = 5;
+        int num2 = 65;
+        int num3 = 4;
+        int sum = num1 + num3 + num2;
+
+        System.out.println(sum);
     }
 }
